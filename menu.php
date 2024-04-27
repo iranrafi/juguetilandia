9ed5fc919612d9813d29a3f8ebf25ce4f45a9e2d
iranrafi <200I0027@tecmartinez.edu.mx>
2024-04-27 12:23:44 -0600

    mi primer cambio

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio5</a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="catalogo.php">Catálogo</a>
